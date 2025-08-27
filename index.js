2f133df751ee4dd3ab5f52ece18427fa594d11a9
QUEZIA MIRANDA PINHEIRO <quezia.mpinheiro@senacsp.edu.br>
2025-08-27 15:54:31 -0300

    USANDO EJS

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
+// Conexão com o bd
+conexao.connect(function(error){
+    if(error) {
+        console.error("Erro ao conectar ao BD")
+        process.exit(); //encerrar o servidor em caso a conexão falhe 
+    };
+});
+
 app.get('/', function(req,res){
     res.sendFile(__dirname+'/cadastro.html'); // get so carrega a pagina
 });
@@ -20,38 +28,38 @@ app.post('/', function(req, res){  // post envia os dados para o banco
     var email = req.body.email;
     var senha = req.body.senha;
 
-    conexao.connect(function(error){
-    if(error) throw error;
-
+    
     // prevenindo SQL Injection
 
     var sql = "INSERT INTO estudante(nomecompleto,email,senha) VALUES (?,?,?)"; //PREVINE QUE INVADEM AS INFORMAÇÕES CONFIDENCIAIS
         conexao.query(sql,[nomecompleto, email, senha], function(error,result){
         if(error) throw error;
 
-        res.send("Estudante cadastrado com sucesso" + result.insertId);
+        //res.send("Estudante cadastrado com sucesso" + result.insertId);
+
+        res.redirect('/estudantes')
     });
 
 });
 
 
 
-});
+
 
 
 
 // Leitura do BD
 app.get('/estudantes', function(req, res){ // req requisicao res resposta
-    conexao.connect(function(error){
-        if(error) console.log(error);
+    
 
         var sql = "select * from estudante";
         conexao.query(sql,function(error, result){
             if(error) console.log(error);
-            console.log(result);
+            //console.log(result);
+            res.render(__dirname+"/estudantes", {estudante:result}); 
         });
     });
-});
+
 
 
 app.listen(7000);
